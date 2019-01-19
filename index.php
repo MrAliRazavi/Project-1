34dbc46d4b9bce58c33919db77f9b0a772a55653
MrAliRazavi <sr.alirazavi@gmail.com>
2019-01-19 13:17:38 +0330

    Fainal Project Ui V1-0.0

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>وب سایت شیپور |  درج آگاهی بازرگانی </title>
+    <title>وب سایت شیپور | درج آگاهی بازرگانی </title>
     <link rel="stylesheet" href="assets/css/font-awesome.css">
     <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 <body>
+<!--Start Header-->
 <div id="header">
     <div id="section-one">
         <span id="logo"><img src="assets/img/logo.png" alt=""></span>
@@ -19,16 +20,92 @@
         <ul>
             <li><a href="#">همه آگهی ها</a></li>
             <span>|</span>
-            <li><a href="#">حساب من</a></li>
+            <li><a href="#"><i class="fa fa-user"></i>حساب من</a></li>
         </ul>
 
     </div>
     <div id="section-three">
         <ul>
             <li><a href="#">پشتیبانی</a></li>
-            <li><a href="#"><button class="btn_sub">+ثبت آگهی رایگان</button></a></li>
+            <li><a href="#">
+                    <button class="btn_sub">+ثبت آگهی رایگان</button>
+                </a></li>
         </ul>
     </div>
 </div>
+<!--End Header-->
+
+<div class="clearfix"></div>
+
+<!--Titr-->
+
+<div id="titr">
+    <h2 class="title-titr">نیاز مندی های رایگان شیپور</h2>
+    <p class="lead-titr">
+        خرید و فروش خودرو، املاک، آپارتمان، گوشی موبایل، تبلت، لوازم خانگی، لوازم دست دوم، استخدام و هر چه فکر کنید!
+    </p>
+    <span class="titr-footer">100 % رایگان خواهد بود و خواهد ماند</span>
+</div>
+
+<!--End Tite-->
+
+<!--Start Map-->
+<div id="map">
+
+</div>
+
+<!--End Map-->
+
+<!--Start Footer-->
+<footer id="footer">
+    <div id="foot-section-top">
+
+        <div id="foot-sec-first">
+            <ul>
+                <li><a href="#">شیپور</a></li>
+                <li><a href="#">درباره شیپور</a></li>
+                <li><a href="#">بلاگ</a></li>
+            </ul>
+        </div>
+
+
+        <div id="foot-sec-center">
+            <ul>
+                <li><a href="#">راهنمای مشتریان</a></li>
+                <li><a href="#">قوانین</a></li>
+            </ul>
+
+        </div>
+
+        <div id="foot-sec-last">
+            <ul>
+                <li><a href="#">خدمات مشتریان</a></li>
+                <li><a href="#">پشتیبانی</a></li>
+                <li><a href="#">ارسال پیشنهادات</a></li>
+            </ul>
+        </div>
+
+        <div class="clearfix"></div>
+
+    </div>
+
+
+    <div id="foot-section-bottom">
+        <p class="text-center">
+           این وب سایت توسط : علی رضوی ساخته شده
+
+        </p>
+
+        <ul id="social">
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa fa-facebook-official"></i></a></li>
+            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+            <li><a href="#"><i class="fa fa-telegram"></i></a></li>
+            <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
+        </ul>
+    </div>
+
+</footer>
+<!--End Footer-->
 </body>
 </html>
